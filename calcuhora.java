ae0149f04f74a453e5675588d1e73d1584fc83b1
gustavo12345765 <141268787+gustavo12345765@users.noreply.github.com>
2023-08-09 08:27:18 -0300

    Create calcuhora.java

---
+++ calcuhora.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+
+class CalcularHoras{
+    public static void  main(String[] args) {
+
+     //cria um objeto que sera utilizado para criar o id de consulta
+     Random consulta = new Random(); 
